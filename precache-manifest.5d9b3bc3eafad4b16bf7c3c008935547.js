6462e133a86961ae4d55378a688cc2fad1f721fa
KosBelozyorov <KosBelozyorov@gmail.com>
2019-09-06 17:27:35 +0300

    Updates

---
+++ precache-manifest.5d9b3bc3eafad4b16bf7c3c008935547.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16692ac30b818ed8a49f9ab21a6d9d3f",
+    "url": "/react_decompose/index.html"
+  },
+  {
+    "revision": "fce1501e9a68361330e3",
+    "url": "/react_decompose/static/css/main.d29ecc8b.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/react_decompose/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "fce1501e9a68361330e3",
+    "url": "/react_decompose/static/js/main.12e8e6ed.chunk.js"
+  },
+  {
+    "revision": "e966956942911a68aecd",
+    "url": "/react_decompose/static/js/runtime~main.f15862fb.js"
+  }
+]);
